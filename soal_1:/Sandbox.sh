b662ffc9c11568ad4753980cec8929b222c92c91
iryandae <endia.iryanda@gmail.com>
2024-03-22 23:07:05 +0700

    commit

--- soal_1:/Sandbox.sh
+++ soal_1:/Sandbox.sh
@@ -4,17 +4,17 @@ echo "top sales customer: "
 sort -t, -k17,17nr Sandbox.csv  | cut -d, -f6 | head -n 1
 echo -e "\n"
 
-echo "least profit customer: "
-sort -t, -k20,20 Sandbox.csv | cut -d, -f6 | head -n 1
+echo "least profit: "
+sort -t, -k20,20 Sandbox.csv | cut -d, -f7 | head -n 1
 echo -e "\n"
 
-echo "most profitable items: "
-sort -t, -k20,20 Sandbox.csv | cut -d, -f16 | head -n 3
+echo "profitable categories: "
+awk -F',' '{arr[$14]+=$20} END {for (i in arr) print i, arr[i]}' Sandbox.csv | sed '3d'
 echo -e "\n"
 
 echo "enter name to search: "
 read search
-no_baris=$(grep -n "$search" Sandbox.csv | cut -d: -f1)
+no_baris=$(grep -n -i "$search" Sandbox.csv | cut -d: -f1)
 echo -e "\ndata found\n"
 echo "purchase date: "
 cut -d, -f2 Sandbox.csv | head -"$no_baris" | tail -n 1
